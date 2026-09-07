5edc30c1c2906c52d0269a35c54be51c50cf0f88
Josh Ruben <198603137+Josh-Ruben@users.noreply.github.com>
2026-09-07 17:33:57 +0530

    Import main module conditionally based on OS

--- facemash/__main__.py
+++ facemash/__main__.py
@@ -1,4 +1,10 @@
-from facelint.app import main
+import platform
+
+if platform.system() == "Windows":
+    from facemash.windows_app import main
+else:
+    from facemash.app import main
+
 
 if __name__ == "__main__":
     main()
